246da12363b631cdc9232471f617d4363b264db0
germandefrancesco <germandefrancesco@hotmail.com>
2023-03-29 11:04:18 -0300

    Fix Snackbar prevent duplicates hiding additional messages

--- Blazor.GlobalErrorHandling.Demo/Program.cs
+++ Blazor.GlobalErrorHandling.Demo/Program.cs
@@ -1,30 +1,26 @@
 using Blazor.GlobalErrorHandling.Demo.Data;
+using MudBlazor;
 using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
-builder.Services.AddMudServices();
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomRight;
+    config.SnackbarConfiguration.PreventDuplicates = false;
+    config.SnackbarConfiguration.ShowCloseIcon = true;
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
+app.UseExceptionHandler("/Error");
+app.UseHsts();
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
